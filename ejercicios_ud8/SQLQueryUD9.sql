1440c3f73b2805f6daaf379b5160abc857d29662
ManuelaPlanelles <manuelaplanelles@gmail.com>
2026-04-20 18:46:11 +0200

    Actualizar SQLQueryUD9.sql

--- ejercicios_ud8/SQLQueryUD9.sql
+++ ejercicios_ud8/SQLQueryUD9.sql
@@ -515,3 +515,137 @@ select * from MEMBER
 
 -------------------------------------------------------------------------------------------------------
 -- 30. TRIG_INSERT_PRODUCT. Write a trigger to check if the price is lower than cost_price.
+
+--2
+create or alter trigger CALCULATE_TIME
+on ROUTE
+after UPDATE
+as
+begin
+    declare @idroute int=(select idroute from inserted)
+    declare @endtimeold time=(select endtime from deleted)
+    declare @endtimenew time=(select endtime from inserted)
+    declare @starttime time=(select starttime from inserted)
+    declare @iddriver int=(select iddriver from inserted)
+    declare @idvehicle int=(select idvehicle from inserted)
+    declare @km int=(select km from inserted)
+    declare @hourlyrate smallmoney=(select hourlyrate from drivers where iddriver=@iddriver)
+    declare @date date=(select dateroute from inserted)
+    declare @totaltime decimal(6,2)
+    declare @hoursworking decimal(4,2)
+
+    if @endtimenew is not NULL  --endtime is updated
+    begin
+        --car and driver are available
+        update DRIVERS
+        set status='Available'
+        where iddriver=@iddriver
+
+        update VEHICLES
+        set statusveh='Available'
+        where idvehicle=@idvehicle
+
+        --update the time
+        if @starttime<@endtimenew   --the driver arrived before midnight
+            set @totaltime=datediff(minute,@starttime,@endtimenew)
+        else    --the driver arrived after midnight
+            begin
+                set @totaltime=datediff(minute,@starttime,@endtimenew)+1440
+            end
+
+        --update totaltime in the table ROUTE
+        update route
+        set totaltime=@totaltime
+        where idroute=@idroute
+
+        --calculate salary --> la ida al precio hora y la vuelta a la mitad
+        if @iddriver in (select iddriver from salaries
+                where monthsal=month(@date) and yearsal=year(@date))
+        begin
+            update SALARIES
+            set salary=salary+(@totaltime/120)*@hourlyrate+
+                (@totaltime/120)*(@hourlyrate/2)
+            where iddriver=@iddriver and monthsal=month(@date) and
+                yearsal=year(@date)
+        end
+
+        else
+        begin
+            insert into SALARIES
+            values (month(@date), year(@date), @iddriver,
+                (@totaltime/120)*@hourlyrate+(@totaltime/120)*(@hourlyrate/2))
+        end
+
+        --update the km in the table KILOMETERS
+        if @idvehicle in (select idvehicle from kilometers)
+        begin
+            update KILOMETERS
+            set TotalKm=TotalKm+@km
+            where idvehicle=@idvehicle
+        end
+        else
+        begin
+            insert into KILOMETERS
+            values (@idvehicle, @km)
+        end
+
+    end
+end
+--3 
+create or alter trigger VEHICLE_SERVICE
+on KILOMETERS
+after update
+as
+begin
+    declare @idvehicle int = (select IDVehicle from inserted)
+    declare @totalkm int = (select TotalKm from inserted)
+
+    -- Si llega a 40.000 km o más → ON SALE
+    if @totalkm >= 40000
+    begin
+        update VEHICLES
+        set StatusVeh = 'On Sale'
+        where IDVehicle = @idvehicle
+    end
+    -- Si llega a 10.000, 20.000 o 30.000 → alerta + INACTIVE
+    else if @totalkm >= 10000 and @totalkm < 40000
+    begin
+        -- Comprobar si es uno de los umbrales de servicio
+        if (@totalkm >= 10000 and @totalkm < 20000)
+            or (@totalkm >= 20000 and @totalkm < 30000)
+            or (@totalkm >= 30000 and @totalkm < 40000)
+        begin
+            insert into ALERTS_MAINTENANCE (IDVehicle, DateAlert)
+            values (@idvehicle, GETDATE())
+
+            update VEHICLES
+            set StatusVeh = 'Inactive'
+            where IDVehicle = @idvehicle
+        end
+    end
+end
+-- 4
+create or alter trigger FINISH_SERVICE
+on MAINTENANCE
+after insert
+as
+begin
+    declare @idvehicle int = (select IDVehicle from inserted)
+    declare @maintype varchar(50) = (select MainType from inserted)
+    declare @datemaint date = (select DateMaint from inserted)
+
+    -- Solo actúa si es tipo 'Service'
+    if @maintype = 'Service'
+    begin
+        -- Actualizar la alerta más reciente sin fecha de mantenimiento (la pendiente)
+        update ALERTS_MAINTENANCE
+        set DateMaintenance = @datemaint
+        where IDVehicle = @idvehicle
+          and DateMaintenance is null
+
+        -- Cambiar estado del vehículo a Available
+        update VEHICLES
+        set StatusVeh = 'Available'
+        where IDVehicle = @idvehicle
+    end
+end
